8f6e3ce0f260d5909b242b0e3c176d1677085cff
Juan Tenezaca <tenj34@gmail.com>
2018-07-28 16:25:04 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -79,9 +79,9 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-}
+			}
 
- programmer_hash = {:yukihiro_matsumoto => {
+ 	programmer_hash = {:yukihiro_matsumoto => {
 		    :known_for => "Ruby",
 		    :languages => ["LISP", "C"]
 			 }
